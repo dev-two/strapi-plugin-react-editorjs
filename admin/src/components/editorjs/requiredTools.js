224b81ee119ca7d16e3c1c86908ab5f203ea3b74
dev-two <metadevone@gmail.com>
2022-10-25 21:25:05 +0200

    Try to rever action

--- admin/src/components/editorjs/requiredTools.js
+++ admin/src/components/editorjs/requiredTools.js
@@ -13,14 +13,14 @@ const requiredTools = {
       additionalRequestData: {
         data: JSON.stringify({}),
       },
-      actions: [
-        {
-          name: "show_create_button",
-          icon: '<svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M15.7 5.3l-1-1c-0.2-0.2-0.4-0.3-0.7-0.3h-13c-0.6 0-1 0.4-1 1v5c0 0.3 0.1 0.6 0.3 0.7l1 1c0.2 0.2 0.4 0.3 0.7 0.3h13c0.6 0 1-0.4 1-1v-5c0-0.3-0.1-0.5-0.3-0.7zM14 10h-13v-5h13v5z"></path></svg>',
-          title: "Show Create Button",
-          action: (name) => true,
-        },
-      ],
+      // actions: [
+      //   {
+      //     name: "show_create_button",
+      //     icon: '<svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M15.7 5.3l-1-1c-0.2-0.2-0.4-0.3-0.7-0.3h-13c-0.6 0-1 0.4-1 1v5c0 0.3 0.1 0.6 0.3 0.7l1 1c0.2 0.2 0.4 0.3 0.7 0.3h13c0.6 0 1-0.4 1-1v-5c0-0.3-0.1-0.5-0.3-0.7zM14 10h-13v-5h13v5z"></path></svg>',
+      //     title: "Show Create Button",
+      //     action: (name) => true,
+      //   },
+      // ],
       additionalRequestHeaders: {
         Authorization: `Bearer ${auth.getToken()}`,
       },
